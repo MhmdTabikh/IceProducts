3351d7be4392b80f588b5ca05c1165749006fc6c
Abbass Tabikh <abbasstabikh87774@gmail.com>
2023-08-20 18:37:12 +0300

    wtf

---
+++ IceProducts/Shared/Dtos/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceProducts.Shared.Dtos
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string SmallDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string Sizes { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
---
+++ IceProducts/Shared/InputModels/UserInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IceProducts.Shared.InputModels
+{
+	public class UserInputModel
+	{
+		[EmailAddress]
+		public string Email { get; set; } = string.Empty;
+
+		[Required]
+		public string Password { get; set; } = string.Empty;
+    }
+}
